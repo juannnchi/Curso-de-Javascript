5b24be828bda4cdfa16f06705e346dee69cbbfac
juannnchi <juanchi.montagni@gmail.com>
2022-07-04 16:06:27 -0300

    se agrega el primer desafio

---
+++ main.js
@@ -0,0 +1,5 @@
+let ingresarNumero = parseInt(prompt("Ingresar numero"));
+for (let i = 1; i <= 10; i++) {
+    let resultado = ingresarNumero * i;
+    alert(ingresarNumero + " X "+ i +" = "+ resultado);
+}
