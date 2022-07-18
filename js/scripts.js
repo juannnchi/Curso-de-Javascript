56c1040422d2dd517a13b79bc26cd49685bd5d65
juannnchi <juanchi.montagni@gmail.com>
2022-07-18 20:44:45 -0300

    desafio incorporando arrays

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,17 @@
-const producto = {
-    remera: 100,
-    buzo: 300,
-}
+const objeto1 = {producto: "remera", precio: 100,}
+const producto = [objeto1]
+producto.push ({producto: "buzo", precio: 300});
+
+console.log(producto)
 
 let compra = prompt('que desea comprar? remera, buzo')
 
 if (compra == "remera") {
-    alert('el costo de las remeras es de: ' + (producto.remera));
+    alert('el costo de las remeras es de: ' + (objeto1.precio));
 }
 
 else if (compra == "buzo") {
-    alert('el costo de los buzos es de: ' + (producto.buzo));
+    alert('el costo de los buzos es de: ' + producto[1]);
 }
 
 let cantidad = parseInt(prompt('que cantidad desea llevar'));
@@ -25,13 +26,13 @@ let resultado
 
 if (compra == "remera") {
     for (let i = 1; i <= cantidad; i++) {
-        resultado = i*producto.remera;
+        resultado = i*objeto1.precio;
     }
 }
 
 else if (compra == "buzo") {
     for (let i = 1; i <= cantidad; i++) {
-        resultado = i*producto.buzo;
+        resultado = i*producto[1];
     }
 }
 
@@ -44,25 +45,24 @@ let medioDePago = prompt('abona en efectivo o tarjeta');
 
 if (medioDePago == "tarjeta") {
     alert('tiene hasta 3 cuotas sin interes, puede hasta 12')
-}
-    
-else {
-    alert('serian $' + pagar)
-}
 
-let cuotas = parseInt(prompt('en cuantas cuotas desea abonar?'))
-let interes = 0.15;
-let pagoEnCuotas = pagar/cuotas;
-let totalAPagar = pagar*interes + pagoEnCuotas;
+    let cuotas = parseInt(prompt('en cuantas cuotas desea abonar?'))
+    let pagoEnCuotas = pagar/cuotas;
+    let interes = 0.15;
+    let totalAPagar = pagar*interes + pagoEnCuotas;
 
-if (cuotas != 3) {
+    if (cuotas != 3) {
     totalAPagar;
-    alert('con un recargo del 15%, le quedarian ' + cuotas + ' cuotas ' + 'con un valor de $' + totalAPagar)
-}
-
-else {
+        alert('con un recargo del 15%, le quedarian ' + cuotas + ' cuotas ' + 'con un valor de $' + totalAPagar)
+    }
+    
+    else {
     alert('le quedarian ' + cuotas + ' cuotas ' + 'con un valor de $' + pagoEnCuotas);
-}
+    }
+} 
 
+else if (medioDePago == "efectivo") {
+    alert ("muchas gracias por su compra")
+}
 
 alert('gracias por su compra')
